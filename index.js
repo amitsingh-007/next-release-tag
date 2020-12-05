7b06ac9825394858ed6a5e95a0610cc52d89564f
cw-amitsingh <51703039+cw-amitsingh@users.noreply.github.com>
2020-12-05 22:39:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,21 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 const generateNextReleaseTag = async () => {
-  const github_token = core.getInput("github_token");
-  const octokit = github.getOctokit(github_token);
-  const { owner, repo } = github.context.repo;
-  const { name, tag_name } = await octokit.repos.getLatestRelease({
-    owner,
-    repo,
-  });
-  console.log(`name: ${name}`);
-  console.log(`tag_name: ${tag_name}`);
-  core.setOutput("previous_tag", tag_name);
-  core.setOutput("new_tag", tag_name);
-  core.exportVariable("release_tag", tag_name);
+  try {
+    const github_token = core.getInput("github_token");
+    const octokit = github.getOctokit(github_token);
+    const { owner, repo } = github.context.repo;
+    const { name, tag_name } = await octokit.repos.getLatestRelease({
+      owner,
+      repo,
+    });
+    console.log(`name: ${name}`);
+    console.log(`tag_name: ${tag_name}`);
+    core.setOutput("previous_tag", tag_name);
+    core.setOutput("new_tag", tag_name);
+    core.exportVariable("release_tag", tag_name);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 };
-
-try {
-  generateNextReleaseTag();
-  core.exportVariable("release_tag", "v1.0.8")
-} catch (error) {
-  core.setFailed(error.message);
-}
+generateNextReleaseTag();
