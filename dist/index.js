c04bc818c3e16d9e7457ccd81c8a47b1ffc8dcaa
cw-amitsingh <51703039+cw-amitsingh@users.noreply.github.com>
2020-12-05 23:18:51 +0530

    Added logic for new release tag

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,34 @@ module.exports =
 const core = __webpack_require__(186);
 const github = __webpack_require__(438);
 
+const generateNewTagFromOld = (oldYear, oldMonth, oldItr) => {
+  const curDate = new Date();
+  const curMonth = curDate.getMonth();
+  const curYear = curDate.getFullYear() % 100;
+  let newYear = curYear;
+  let newMonth = curMonth;
+  let newItr = oldItr + 1;
+  if (curMonth !== oldMonth) {
+    newItr = 1;
+    newMonth = curMonth;
+  }
+  if (curYear != oldYear) {
+    newYear = curYear;
+  }
+  return `v${newYear}.${newMonth}.${newItr}`;
+};
+
+const getNewReleaseTag = (oldReleaseTag) => {
+  if (oldReleaseTag & oldReleaseTag.startsWith("v")) {
+    const [oldYear, oldMonth, oldItr] = oldReleaseTag
+      .substring(1)
+      .split(".")
+      .map((x) => Number(x));
+    return generateNewTagFromOld(oldYear, oldMonth, oldItr);
+  }
+  return generateNewTagFromOld(-1, -1, -1);
+};
+
 const generateNextReleaseTag = async () => {
   try {
     const github_token = core.getInput("github_token");
@@ -17,12 +45,11 @@ const generateNextReleaseTag = async () => {
       owner,
       repo,
     });
-    const { name, tag_name } = response.data;
-    console.log(`name: ${name}`);
-    console.log(`tag_name: ${tag_name}`);
-    core.setOutput("previous_tag", tag_name);
-    core.setOutput("new_tag", tag_name);
-    core.exportVariable("release_tag", tag_name);
+    const { tag_name: oldReleaseTag } = response.data;
+    const newReleaseTag = getNewReleaseTag();
+    console.log(`Previous Release Tag: ${oldReleaseTag}`);
+    console.log(`New Release Tag: ${newReleaseTag}`);
+    core.exportVariable("release_tag", newReleaseTag);
   } catch (error) {
     core.setFailed(error.message);
   }
