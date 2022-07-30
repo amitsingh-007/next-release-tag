a497820bb679b3b22b495ced6c105438b2d477a9
Amit Singh <amitsingh5198@gmail.com>
2022-07-30 19:24:44 +0530

    Add tests (#4)

--- dist/index.js
+++ dist/index.js
@@ -8833,6 +8833,42 @@ function wrappy (fn, cb) {
 
 /***/ }),
 
+/***/ 1205:
+/***/ ((module) => {
+
+const generateNewTagFromOld = (oldYear, oldMonth, oldItr) => {
+  const curDate = new Date();
+  const curMonth = curDate.getMonth() + 1;
+  const curYear = curDate.getFullYear() % 100;
+  let newYear = curYear;
+  let newMonth = curMonth;
+  let newItr = oldItr + 1;
+  if (curMonth !== oldMonth) {
+    newItr = 1;
+    newMonth = curMonth;
+  }
+  if (curYear != oldYear) {
+    newYear = curYear;
+  }
+  return `v${newYear}.${newMonth}.${newItr}`;
+};
+
+const getNewReleaseTag = (oldReleaseTag) => {
+  if (oldReleaseTag && oldReleaseTag.startsWith("v")) {
+    const [oldYear, oldMonth, oldItr] = oldReleaseTag
+      .substring(1)
+      .split(".")
+      .map((x) => Number(x));
+    return generateNewTagFromOld(oldYear, oldMonth, oldItr);
+  }
+  return generateNewTagFromOld(-1, -1, -1);
+};
+
+module.exports = getNewReleaseTag;
+
+
+/***/ }),
+
 /***/ 2877:
 /***/ ((module) => {
 
@@ -9004,34 +9040,7 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
-
-const generateNewTagFromOld = (oldYear, oldMonth, oldItr) => {
-  const curDate = new Date();
-  const curMonth = curDate.getMonth() + 1;
-  const curYear = curDate.getFullYear() % 100;
-  let newYear = curYear;
-  let newMonth = curMonth;
-  let newItr = oldItr + 1;
-  if (curMonth !== oldMonth) {
-    newItr = 1;
-    newMonth = curMonth;
-  }
-  if (curYear != oldYear) {
-    newYear = curYear;
-  }
-  return `v${newYear}.${newMonth}.${newItr}`;
-};
-
-const getNewReleaseTag = (oldReleaseTag) => {
-  if (oldReleaseTag && oldReleaseTag.startsWith("v")) {
-    const [oldYear, oldMonth, oldItr] = oldReleaseTag
-      .substring(1)
-      .split(".")
-      .map((x) => Number(x));
-    return generateNewTagFromOld(oldYear, oldMonth, oldItr);
-  }
-  return generateNewTagFromOld(-1, -1, -1);
-};
+const getNewReleaseTag = __nccwpck_require__(1205);
 
 const generateNextReleaseTag = async () => {
   try {
--- src/getReleaseTag.js
+++ src/getReleaseTag.js
@@ -1,6 +1,3 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
-
 const generateNewTagFromOld = (oldYear, oldMonth, oldItr) => {
   const curDate = new Date();
   const curMonth = curDate.getMonth() + 1;
@@ -29,23 +26,4 @@ const getNewReleaseTag = (oldReleaseTag) => {
   return generateNewTagFromOld(-1, -1, -1);
 };
 
-const generateNextReleaseTag = async () => {
-  try {
-    const github_token = core.getInput("github_token");
-    const octokit = github.getOctokit(github_token);
-    const { owner, repo } = github.context.repo;
-    const response = await octokit.rest.repos.getLatestRelease({
-      owner,
-      repo,
-    });
-    const { tag_name: oldReleaseTag } = response.data;
-    const newReleaseTag = getNewReleaseTag(oldReleaseTag);
-    console.log(`Previous Release Tag: ${oldReleaseTag}`);
-    console.log(`New Release Tag: ${newReleaseTag}`);
-    core.exportVariable("release_tag", newReleaseTag);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
-};
-
-generateNextReleaseTag();
+module.exports = getNewReleaseTag;
