b76d34b51a9dd08c9f440505ca559f6dd1923084
Amit Singh <amitsingh5198@gmail.com>
2022-09-29 21:25:23 +0530

    asd

--- src/index.js
+++ src/index.js
@@ -7,22 +7,17 @@ const generateNextReleaseTag = async () => {
     const github_token = getInput("github_token");
     const octokit = getOctokit(github_token);
     const { owner, repo } = context.repo;
-    console.log("Before call");
     const response = await octokit.rest.repos.getLatestRelease({
       owner,
       repo,
     });
-    console.log("Response", response);
     const { tag_name: oldReleaseTag } = response.data;
     const newReleaseTag = getNewReleaseTag(oldReleaseTag);
     console.log(`Previous Release Tag: ${oldReleaseTag}`);
     console.log(`New Release Tag: ${newReleaseTag}`);
     exportVariable("release_tag", newReleaseTag);
   } catch (error) {
-    console.log(error);
-    console.log(JSON.stringify(error));
-    console.log(error.response);
-    console.log(error.data);
+    console.log(error.response.status);
     setFailed(error);
   }
 };
