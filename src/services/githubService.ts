1943e84f1e186db5ad5272b9bc67d4875f42b216
Amit Singh <amitsingh5198@gmail.com>
2023-06-02 20:21:55 +0530

    Fetch latest tag instead of latest published full release

--- src/services/githubService.ts
+++ src/services/githubService.ts
@@ -2,20 +2,15 @@ import { getInput } from '@actions/core';
 import { context, getOctokit } from '@actions/github';
 
 export const fetchLatestReleaseTag = async () => {
-  try {
-    const githubToken = getInput('github_token', { required: true });
-    const octokit = getOctokit(githubToken);
-    const { owner, repo } = context.repo;
-    const response = await octokit.rest.repos.getLatestRelease({
-      owner,
-      repo,
-    });
-    return response.data.tag_name;
-  } catch (error: any) {
-    // No releases yet
-    if (error?.response?.status === 404) {
-      return null;
-    }
-    throw error;
-  }
+  const githubToken = getInput('github_token', { required: true });
+  const octokit = getOctokit(githubToken);
+  const { owner, repo } = context.repo;
+  // Fetch only latest tag
+  const response = await octokit.rest.repos.listTags({
+    owner,
+    repo,
+    page: 1,
+    per_page: 1,
+  });
+  return response.data.at(0)?.name;
 };
