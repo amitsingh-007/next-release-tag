9321315ed07a05b9a7139c990da3076c0191d350
Amit Singh <amitsingh5198@gmail.com>
2023-06-02 20:29:18 +0530

    Add error logging

--- src/services/githubService.ts
+++ src/services/githubService.ts
@@ -2,15 +2,20 @@ import { getInput } from '@actions/core';
 import { context, getOctokit } from '@actions/github';
 
 export const fetchLatestReleaseTag = async () => {
-  const githubToken = getInput('github_token', { required: true });
-  const octokit = getOctokit(githubToken);
-  const { owner, repo } = context.repo;
-  // Fetch only latest tag
-  const response = await octokit.rest.repos.listTags({
-    owner,
-    repo,
-    page: 1,
-    per_page: 1,
-  });
-  return response.data.at(0)?.name;
+  try {
+    const githubToken = getInput('github_token', { required: true });
+    const octokit = getOctokit(githubToken);
+    const { owner, repo } = context.repo;
+    // Fetch only latest tag
+    const response = await octokit.rest.repos.listTags({
+      owner,
+      repo,
+      page: 1,
+      per_page: 1,
+    });
+    return response.data?.at(0)?.name;
+  } catch (e) {
+    console.error('Error while fetching tags list for this repository');
+    throw e;
+  }
 };
